090d58cc4d67ececbc4feb005b93ee0d8ee67079
Mostafa <mostafawanies1.2@gmail.com>
2023-01-10 18:52:55 +0200

    10/1 commit

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,13 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         // w that refer to weight
         int w = scanner.nextInt();
-        if (w % 2 == 0){ // that mean w is even
+        if (w > 0 && w <= 100 && w % 2 == 0){ // that mean w is even
             System.out.println("yes");
         }
-        else { // that mean w is odd
+        else if (w > 100){ // that mean w is odd
+            System.out.println("please enter weight between 1 to 100");
+        }
+        else {
             System.out.println("no");
         }
     }
