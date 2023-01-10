5d80f60ab203554543ec9ff79268d7b9a2e7a6a4
Mostafa <mostafawanies1.2@gmail.com>
2023-01-10 18:43:57 +0200

    10/1 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        // w that refer to weight
+        int w = scanner.nextInt();
+        if (w % 2 == 0){ // that mean w is even
+            System.out.println("yes");
+        }
+        else { // that mean w is odd
+            System.out.println("no");
+        }
     }
 }
