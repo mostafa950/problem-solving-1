2d3f644746ea59ff54a14e2e059e5c8f28ea2025
Mostafa <mostafawanies1.2@gmail.com>
2023-01-14 18:36:23 +0200

    14/1 commit

---
+++ src/PUM.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PUM {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numOfLines = scanner.nextInt();
+        String pum = "";
+        int num = 1;
+        for (int i = 1 ; i <= numOfLines ; i++){
+            for (int m = num ; m % 4 != 0 ; m++ ){
+                num = m;
+                pum += num + " ";
+            }
+            System.out.println(pum + "pum");
+            num++;
+            pum = "";
+            if(num % 4 == 0){
+                num++;
+            }
+        }
+    }
+    }
