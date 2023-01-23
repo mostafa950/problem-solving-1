7d10059d466ff0e2af76d1d1d999a6451a6c2d3e
Mostafa <mostafawanies1.2@gmail.com>
2023-01-23 13:15:23 +0200

    23/1 commit

---
+++ src/GoodNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class GoodNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int range = scanner.nextInt();
+        int counter =0;
+        int result =0;
+        for (int i = 0 ; i < size ; i++){
+            String num = scanner.next();
+            for(int x = 0 ; x < num.length() ; x++) {
+                counter = 0;
+                for(int z = 0 ; z <= range ; z++){
+                    if(num.contains(String.valueOf(z))){
+                        counter++;
+                    }
+                }
+
+            }
+            if(counter == range+1){
+                result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
